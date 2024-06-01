ed1818bd1e9658cb1b67b78dc59ba3d75daf09e7
chloe <s10256056@connect.np.edu.sg>
2024-06-02 00:28:06 +0800

    Chloe: Put Marissa's feature

--- app/src/main/java/sg/edu/np/mad/p04_team4/ForgetPasswordActivity.java
+++ app/src/main/java/sg/edu/np/mad/p04_team4/ForgetPasswordActivity.java
@@ -1,7 +1,7 @@
 package sg.edu.np.mad.p04_team4;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,25 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ForgetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.forgot_password);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Intent challange_made= new Intent(MainActivity.this,Friendship_Events.class);
-        startActivity(challange_made);
 
-//test
-
-
-
-
+        Toast.makeText(ForgetPasswordActivity.this, "Password successfully changed.", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/sg/edu/np/mad/p04_team4/LoginActivity.java
@@ -0,0 +1,27 @@
+package sg.edu.np.mad.p04_team4;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.loginpage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Toast.makeText(LoginActivity.this, "Login successfully.", Toast.LENGTH_SHORT).show();
+    }
+}
