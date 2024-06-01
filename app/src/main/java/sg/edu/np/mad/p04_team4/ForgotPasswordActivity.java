0395b4e03c7d4d4293ac35d962cbeb3a8c83cf0f
xXta1ibanSniper420Xx <osama95612@gmail.com>
2024-06-02 04:38:38 +0800

    toilet

---
+++ app/src/main/java/sg/edu/np/mad/p04_team4/ForgotPasswordActivity.java
@@ -0,0 +1,22 @@
+package sg.edu.np.mad.p04_team4;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgot_password_number);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
+
