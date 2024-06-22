b253a16abc17cff85669698dad6e53ab767f8af4
chloe <s10256056@connect.np.edu.sg>
2024-06-22 18:42:20 +0800

    chloe: update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.1" //update version name
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
