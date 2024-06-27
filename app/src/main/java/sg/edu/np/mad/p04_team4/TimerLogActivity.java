4777d83c7f1bad708aa37a5fda80e09c7dbbb14e
chloe <s10256056@connect.np.edu.sg>
2024-06-27 10:56:19 +0800

    poop

--- app/src/main/java/sg/edu/np/mad/p04_team4/TimerLogActivity.java
+++ app/src/main/java/sg/edu/np/mad/p04_team4/TimerLogActivity.java
@@ -2,11 +2,7 @@ package sg.edu.np.mad.p04_team4;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
